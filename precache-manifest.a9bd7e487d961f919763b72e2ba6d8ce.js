3ffd0f002e2a85c95dd358a3da19e06e9e4d15a5
Sanskar Patni <sanskarpatni28@gmail.com>
2020-06-27 15:44:27 +0530

    Updates

---
+++ precache-manifest.a9bd7e487d961f919763b72e2ba6d8ce.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfce75e202c178f629e90773a555a073",
+    "url": "/ktjassignment3reactTAapp/index.html"
+  },
+  {
+    "revision": "931a2f99202ddf8c4247",
+    "url": "/ktjassignment3reactTAapp/static/css/main.fbbd9ebf.chunk.css"
+  },
+  {
+    "revision": "6450b5ad21c1d7839c72",
+    "url": "/ktjassignment3reactTAapp/static/js/2.b9d4a0aa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ktjassignment3reactTAapp/static/js/2.b9d4a0aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "931a2f99202ddf8c4247",
+    "url": "/ktjassignment3reactTAapp/static/js/main.876bff67.chunk.js"
+  },
+  {
+    "revision": "07ec26d6cbe57c2c78be",
+    "url": "/ktjassignment3reactTAapp/static/js/runtime-main.3f704ac4.js"
+  },
+  {
+    "revision": "c4edc25924b0caf45acf48cc4125cfe9",
+    "url": "/ktjassignment3reactTAapp/static/media/Bubai_Manna_(IIT_Kharagpur_main_building)-min (1)-1579328628.c4edc259.jpg"
+  },
+  {
+    "revision": "eb9902991926a76549c629791e14415b",
+    "url": "/ktjassignment3reactTAapp/static/media/logo.eb990299.png"
+  }
+]);
